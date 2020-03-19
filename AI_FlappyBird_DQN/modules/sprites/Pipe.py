ef1a3c91aa60c76d50ac9dade335122de03fcd06
Fowindy <470026339@qq.com>
2020-03-19 17:37:35 +0800

    新建pipe class类继承精灵序列图实现动画

--- AI_FlappyBird_DQN/modules/sprites/Pipe.py
+++ AI_FlappyBird_DQN/modules/sprites/Pipe.py
@@ -13,3 +13,5 @@ github:
 import os #导入os模块：负责程序和操作系统之间的交互
 import random # 导入random模块:用于生成管道中的随机变量
 import pygame # 导入pygame模块:用于管道界面的设计和显示
+'''pipe class'''
+class Pipe(pygame.sprite.Sprite) # 新建pipe class类继承精灵序列图实现动画
