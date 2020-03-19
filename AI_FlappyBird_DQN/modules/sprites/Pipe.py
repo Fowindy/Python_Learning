76fdbdd7437cced809a1e52fc357d04e363eb40c
Fowindy <470026339@qq.com>
2020-03-19 16:10:25 +0800

    新建Pipe.py模块

---
+++ AI_FlappyBird_DQN/modules/sprites/Pipe.py
@@ -0,0 +1,13 @@
+﻿'''
+功能描述:
+        定义pipe class类模块
+作者:
+    Fowindy
+微信:
+    17786508658
+github:
+       https://github.com/Fowindy/Python_Learning.git
+创建时间:
+        2020年3月19日 星期四 16:07:30 
+'''
+import os #导入os模块：负责程序和操作系统之间的交互
